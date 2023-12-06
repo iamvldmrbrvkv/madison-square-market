af6ef8dd7f2499e84017192eaaca272467e1ba34
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-12-07 00:29:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('#cart').on('click', () => {
+    $('#cartMenu').show()
+  })
+  $('#account').on('click', () => {
+    $('#accountMenu').show()
+  })
+  $('#help').on('click', () => {
+    $('#helpMenu').show()
+  })
+  $('.dropdown-menu').on('mouseleave', () => {
+    $('.dropdown-menu').hide()
+  })
+})
